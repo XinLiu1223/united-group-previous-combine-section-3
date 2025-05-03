ee7977a1ea304e10fb564d7cfe3bf3f6520f14a8
XinLiu1223 <xinliu1223@gmail.com>
2025-05-03 17:49:21 -0400

    setup auth

--- lib/validators.ts
+++ lib/validators.ts
@@ -24,7 +24,8 @@ export const insertProductSchema = z.object({
   //   rating: currency,
 });
 
+// Schema for signing users in
 export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address"),
-  password: z.string().min(6, "Password must be at least 6 character"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
 });
